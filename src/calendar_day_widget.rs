900e88ba007ec2c1831ef76a38dccca24c3f4edc
liamwb <liam.woodbaker@gmail.com>
2025-01-23 22:41:59 +1100

    stateful calendarday scaffold

--- src/calendar_day_widget.rs
+++ src/calendar_day_widget.rs
@@ -2,7 +2,7 @@ use chrono::{Datelike, NaiveDate};
 use ratatui::{
     buffer::Buffer, 
     layout::Rect, 
-    widgets::{Block, List, Widget}
+    widgets::{Block, List, ListState, StatefulWidget, Widget}
 };
 use crate::event_widget::EventWidget;
 
@@ -21,12 +21,31 @@ impl Widget for CalendarDayWidget<'_> {
         let block = Block::bordered().title(title);
         let list = List::new(&self.events).block(block);
 
-        list.render(area, buf);
+        // render is implemented for both StatefulWidget and Widget, so we need to tell
+        // Rust which one to use explicitly. This is equivalent to list.render(area, buf)
+        Widget::render(list, area, buf);
     }
 }
 
 impl<'a> CalendarDayWidget<'a> {
     pub fn new(events: Vec<EventWidget<'a>>, date: &'a NaiveDate) -> Self {
         CalendarDayWidget { events , date }
+    }
+}
+
+// Extending the CalendarDayWidget so it can store some state:
+// - is it selected?
+// - which event is selected, if an event is ndidate #2 is defined in an impl of the trait `Widget`selected?
+
+pub struct CalendarDayWidgetState {
+    is_selected: bool,
+    list_state: ListState,
+}
+
+impl StatefulWidget for CalendarDayWidget<'_> {
+    type State = CalendarDayWidgetState;
+
+    fn render(self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
+        todo!()
     }
 }
